d3f7741c58a2e135536799950f0ecd4efda57cb2
NoMaidenz <deathblossom247@gmail.com>
2023-01-18 13:14:53 -0500

    items

---
+++ script.js
@@ -0,0 +1,73 @@
+const items = [ 
+{
+    name: "Free 20 min 1:1 VOD",
+    image: "",
+    price: 0,
+    categories: ["1:1", "VOD", "Free"],
+}
+
+
+{ 
+    name: "1:1 Private Vod Review 1hr", 
+    image: "", 
+    price: 9.99,
+    categories: ["1:1", "VOD", "Paid", "1hr"], 
+}, 
+    
+{ 
+    name: "1:1 Private Vod Review 2hr", 
+    image: "", 
+    price: 14.99,
+    categories: ["1:1", "VOD", "Paid", "2hr"], 
+}, 
+
+{ 
+    name: "Team Vod Review 1hr", 
+    image: "",
+    price: 14.99, 
+    categories: ["Team", "VOD", "Paid", "1hr" ], 
+} 
+
+{ 
+    name: "Team Vod Review 2hr", 
+    image: "",
+    price: 24.99, 
+    categories: ["Team", "VOD", "Paid", "2hr" ], 
+} 
+
+{
+    name:"Live 1:1 Coaching 1hr",
+    image:"",
+    price:19.99,
+    categories: ["1:1", "Live", "Paid", "1hr"],
+}
+
+{
+    name:"Live 1:1 Coaching 2hr",
+    image:"",
+    price:34.99,
+    categories: ["1:1", "Live", "Paid", "2hr"],
+}
+
+{
+    name:"Team Scrim Coaching 1hr",
+    image: "",
+    price: 15.99,
+    categories: ["Team", "Live", "Paid", "1hr"],
+}
+
+{
+    name:"Team Scrim Coaching 1hr",
+    image: "",
+    price: 29.99,
+    categories: ["Team", "Live", "Paid", "1hr"],
+}
+
+{
+    name:"",
+    image: "",
+    price: ,
+    categories: ["","",""],
+}
+
+]
